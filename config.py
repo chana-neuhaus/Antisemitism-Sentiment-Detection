e1b8905c1b37c0e91463923832a6011cb00e66e2
Chana Neuhaus <cneuhaus@my365.bellevue.edu>
2026-05-30 23:50:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,7 @@ MODEL_PATH = "models/current_model.pkl"
 # This must match the SentenceTransformer model used to create the
 # embeddings during training. If you trained with a different model,
 # replace this value.
-EMBEDDING_MODEL_NAME = r"C:\Users\chani\.cache\huggingface\hub\models--sentence-transformers--all-MiniLM-L6-v2\snapshots\c9745ed1d9f207416be6d2e6f8de32d1f16199bf"
-
+EMBEDDING_MODEL_NAME =  "sentence-transformers/all-MiniLM-L6-v2"
 # Label mapping. Change these only if your model uses the opposite class order.
 LABELS = {
     0: "Not Biased",
